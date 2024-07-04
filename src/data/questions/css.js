8a852a00799d6fb4127e627b86fb845ec5e4a479
Voichita Maria <sabo_voichita@yahoo.com>
2024-07-04 16:22:15 +0200

    rearrange file

--- src/data/questions/css.js
+++ src/data/questions/css.js
@@ -48,25 +48,21 @@ window.LOAD_QUESTIONS(3, [
     id: 3,
     level: 3,
     text: "What is the CSS selector for an <a> tag containing the title attribute?",
-    type: "css",
     answerType:"radio",
     answers: [
       {
         id: 1,
         text: `a[title]`,
-        type: "css"
       },
       { id: 2, 
         text: `a > title`,
-        type: "css" },
+      },
       {
         id: 3,
         text: `a=title`,
-        type: "css"
       }, {
         id: 4,
         text: `a.title`,
-        type: "css"
       }
     ]
   },
@@ -77,10 +73,10 @@ window.LOAD_QUESTIONS(3, [
     type: "css",
     answerType:"radio",
     q:`
-    .nav {
-    ...;
-    }
-    `,
+      .nav {
+      ...;
+      }
+      `,
     answers: [ 
       "An element with a class of 'nav'",
       "A nav element",
@@ -94,10 +90,10 @@ window.LOAD_QUESTIONS(3, [
     type: "css",
     answerType:"radio",
     q:`
-    #nav {
+      #nav {
       ...;
-    }
-    `,
+      }
+      `,
     answers: [ 
       "An element with a class of 'nav'",
       "A nav element",
@@ -111,10 +107,10 @@ window.LOAD_QUESTIONS(3, [
     type: "css",
     answerType:"radio",
     q:`
-    nav {
+      nav {
       ...;
-    }
-    `,
+      }
+      `,
     answers: [ 
       "An element with a class of 'nav'",
       "A nav element",
@@ -125,38 +121,34 @@ window.LOAD_QUESTIONS(3, [
     id: 7,
     level: 3,
     text: "In this example, according to cascading and specificity rules, what color will the link be?",
-    type: "css",
+    type: "html",
     answerType:"radio",
     q:`
-
-<style>
-    .example {
-  color: yellow;
-}
-ul li a {
-  color: blue;
-}
-ul a {
-  color: green;
-}
-a {
-  color: red;
-}
-</style>  
-
-.....................................................................................................................................................................................
-
-<ul>
-<li><a href="#" class="example">link</a></li>
-<li>list item</li>
-<li>list item</li>
-</ul>
+      <style>
+      .example {
+        color: yellow;
+      }
+      ul li a {
+        color: blue;
+      }
+      ul a {
+        color: green;
+      }
+      a {
+        color: red;
+      }
+      </style>  
+       <ul>
+        <li><a href="#" class="example">link</a></li>
+        <li>list item</li>
+        <li>list item</li>
+      </ul>
     `,
     answers: [ 
- "green",
- "yellow",
- " blue",
- "red"
+      "green",
+      "yellow",
+      " blue",
+      "red"
     ]
   },
 
@@ -172,12 +164,14 @@ a {
         text: `.example a`,
         type: "css"
       },
-      { id: 2, text: `div a`, type: "css" },
+      { id: 2, text: `div a`, type: "css" 
+      },
       {
         id: 3,
         text: `a`,
         type: "css"
-      }, {
+      }, 
+      {
         id: 4,
         text: `ul li a`,
         type: "css"
@@ -210,33 +204,34 @@ a {
       }
     ]
   },
-{
+  {
     id: 3,
     level: 13,
     text: "CSS grid and flexbox are now becoming a more popular way to create page layouts. However, floats are still commonly used, especially when working with an older code base, or if you need to support an older browser version. What are two valid techniques used to clear floats?",
     type: "css",
     answerType:"radio",
     answers: [
-    "Use the 'clearfix hack' on the floated element and add a float to the parent element.",
-    "Use the overflow property on the floated element or the 'clearfix hack' on either the floated or parent element.",
-    "Use the 'clearfix hack' on the floated element or the overflow property on the parent element.",
-    "Use the 'clearfix hack' on the parent element or use the overflow property with a value other than 'visible.'"
+      "Use the 'clearfix hack' on the floated element and add a float to the parent element.",
+      "Use the overflow property on the floated element or the 'clearfix hack' on either the floated or parent element.",
+      "Use the 'clearfix hack' on the floated element or the overflow property on the parent element.",
+      "Use the 'clearfix hack' on the parent element or use the overflow property with a value other than 'visible.'"
     ]
   },
-{
+  {
     id: 4,
     level: 13,
     text: "When adding transparency styles, what is the difference between using the opacity property versus the background property with a rgba() value?",
     type: "css",
     answerType:"radio",
-    answers: ["Opacity specifies the level of transparency of the child elements. Background with a rgba() value applies transparency to the background color only.",
-              "Opacity applies transparency to the background color only. Background with an rgba() value specifies the level of transparency of an element, as a whole, including its content.",
-              "Opacity specifies the level of transparency of an element, including its content. Background with a rgba() value applies transparency to the background color only.",
-              " Opacity applies transparency to the parent and child elements. Background with a rgba() value specifies the level of transparency of the parent element only."
+    answers: [
+      "Opacity specifies the level of transparency of the child elements. Background with a rgba() value applies transparency to the background color only.",
+      "Opacity applies transparency to the background color only. Background with an rgba() value specifies the level of transparency of an element, as a whole, including its content.",
+      "Opacity specifies the level of transparency of an element, including its content. Background with a rgba() value applies transparency to the background color only.",
+      " Opacity applies transparency to the parent and child elements. Background with a rgba() value specifies the level of transparency of the parent element only."
       
     ]
   },
-{
+  {
     id: 5,
     level: 13,
     text: "What is true of block and inline elements? (Alternative: Which statement about block and inline elements is true?)",
@@ -249,24 +244,24 @@ a {
       "A <span> is an example of a block element. <div> is an example of an inline element."
     ]
   },
-{
+  {
     id: 6,
     level: 13,
     text: "CSS grid introduced a new length unit, fr, to create flexible grid tracks. Referring to the code sample below, what will the widths of the three columns be?",
     type: "css",
     answerType:"radio",
-	q:`
-	.grid {
-  	display: grid;
-  	width: 500px;
-  	grid-template-columns: 50px 1fr 2fr;
-	}
-	`,
+	  q:`
+	    .grid {
+  	    display: grid;
+  	    width: 500px;
+  	    grid-template-columns: 50px 1fr 2fr;
+	    }
+	    `,
     answers: [
-    "The first column will have a width of 50px. The second column will be 50px wide and the third column will be 100px wide.",
-    "The first column will have a width of 50px. The second column will be 150px wide and the third column will be 300px wide.",
-    "The first column will have a width of 50px. The second column will be 300px wide and the third column will be 150px wide.",
-    "The first column will have a width of 50px. The second column will be 500px wide and the third column will be 1000px wide.",
+      "The first column will have a width of 50px. The second column will be 50px wide and the third column will be 100px wide.",
+      "The first column will have a width of 50px. The second column will be 150px wide and the third column will be 300px wide.",
+      "The first column will have a width of 50px. The second column will be 300px wide and the third column will be 150px wide.",
+      "The first column will have a width of 50px. The second column will be 500px wide and the third column will be 1000px wide.",
     ]
   },
   {
@@ -276,13 +271,13 @@ a {
     type: "css",
     answerType:"radio",
     q:`
-    .grid { display: grid; grid-template-columns: 50px 1fr 2fr; }
-    `,
+      .grid { display: grid; grid-template-columns: 50px 1fr 2fr; }
+      `,
     answers: [ 
- "50px, 150px, 300px",
- "50px, 200px, 300px",
- "50px, 100px, 200px",
- "50px, 50px, 100px"
+      "50px, 150px, 300px",
+      "50px, 200px, 300px",
+      "50px, 100px, 200px",
+      "50px, 50px, 100px"
     ]
   },
   {
@@ -292,10 +287,10 @@ a {
     type: "css",
     answerType:"radio",
     answers: [ 
- "to control the height of the space between two lines of content",
- "to control the height of the space between heading elements",
- "to control the height of the character size",
- "to control the width of the space between characters"
+      "to control the height of the space between two lines of content",
+      "to control the height of the space between heading elements",
+      "to control the height of the character size",
+      "to control the width of the space between characters"
     ]
   },
   {
@@ -305,10 +300,10 @@ a {
     type: "css",
     answerType:"radio",
     answers: [ 
- "Multiple classes can be used within the same element.",
- "The same class can be used multiple times per page.",
- "Class selectors begin with a leading period(dot)",
- "Classes can be used multiple times per page but not within the same element."
+      "Multiple classes can be used within the same element.",
+      "The same class can be used multiple times per page.",
+      "Class selectors begin with a leading period(dot)",
+      "Classes can be used multiple times per page but not within the same element."
     ]
   },
   {
@@ -318,11 +313,11 @@ a {
     type: "css",
     answerType:"radio",
     answers: [ 
- "Only one class value can be assigned to an element.",
- "An element can have multiple class values.",
- "Class selectors begin with a leading period(dot)",
- "More than one element can have the same class value."
-    ]
+      "Only one class value can be assigned to an element.",
+      "An element can have multiple class values.",
+      "Class selectors begin with a leading period(dot)",
+      "More than one element can have the same class value."
+        ]
   },
   {
     id: 11,
@@ -331,10 +326,10 @@ a {
     type: "css",
     answerType:"radio",
     answers: [ 
- "position",
- "flexbox",
- "grid",
- "float"
+      "position",
+      "flexbox",
+      "grid",
+      "float"
     ]
   },
 ]);
